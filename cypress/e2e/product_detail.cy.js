5d392ba72d3903b6551de18f7ec808366359c51b
Goksu Dag <goksudag@hotmail.com>
2026-04-16 11:30:33 +0100

    Cleanup: Removed unnecessary pom.png file

--- cypress/e2e/product_detail.cy.js
+++ cypress/e2e/product_detail.cy.js
@@ -1,42 +1,28 @@
 import ProductPage from '../support/pages/ProductDetailPage';
 
 describe('US03: Ürün Detay ve Sepet Süreci', () => {
-
     beforeEach(() => {
         cy.on('uncaught:exception', () => false); 
         cy.visit('/', { timeout: 100000 });
-        cy.cleanUI(); // Commands içindeki o güçlü temizleyici
+        cy.cleanUI();
     });
-
     it('AC1-AC6: Tüm Kriterleri Doğrula ve Süreci Bitir', () => {
-        // 1. Ürüne Git (AC1)
         ProductPage.goToFirstProduct();
         ProductPage.productTitle.should('be.visible');
-        
-        // 2. Detay Kontrolü (AC2 & AC3)
         ProductPage.verifyProductDetails();
-
-        // 3. Sepete Ekle (AC4)
         cy.intercept('POST', '**/srv/service/cart/add-to-cart').as('addToCart');
         ProductPage.addToCartBtn.should('be.visible').click({ force: true });
-
-        // 4. API ve Sepet Kontrolü (AC5 & AC6)
         cy.wait('@addToCart', { timeout: 20000 }).then((interception) => {
             expect(interception.response.statusCode).to.eq(200);
-
             cy.wait(2000);
             cy.reload();
             cy.cleanUI();
-
-            // SAYAÇ KONTROLÜ
             ProductPage.basketIcon.should('exist').then(($el) => {
                 const basketText = $el.text();
                 const countMatch = basketText.match(/\d+/);
                 const count = countMatch ? parseInt(countMatch[0]) : 0;
                 expect(count, 'Sepet hala boş!').to.be.at.least(1);
             });
-
-            // Onay Metni
             cy.contains(/sepet|eklendi/i).should('exist');
         });
     });
--- cypress/support/pages/CartPage.js
+++ cypress/support/pages/CartPage.js
@@ -1,13 +1,9 @@
 class CartPage {
-    // Element Seçicileri
     get basketItems() { return cy.get('.cart-item, .product-item-row'); }
     get emptyCartMessage() { return cy.contains('Sepetinizde ürün bulunmamaktadır'); }
     get cartTotal() { return cy.get('.cart-total, .total-price'); }
     get removeProductBtn() { return cy.get('.remove-item, .btn-delete'); }
-    // Eksik olan sepet butonu seçicisi (Genelde sağ üstteki sepet ikonu)
     get cartIcon() { return cy.get('.shopping-cart, #shoppingCart'); } 
-
-    // Çerezleri Kabul Etme
     acceptCookies() {
         cy.get('body').then(($body) => {
             if ($body.find('#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll').length > 0) {
@@ -15,21 +11,14 @@ class CartPage {
             }
         });
     }
-
-    // HATA ALAN METOT: Sepete gitme fonksiyonunu ekledik
     clickCart() {
-        // Eğer bir buton varsa ona tıkla, yoksa doğrudan URL'e git (en güvenli yol)
         cy.visit('/sepet'); 
     }
-
     verifyBasketIsNotEmpty() {
         this.basketItems.should('have.length.at.least', 1);
     }
-
    verifyEmptyCart() {
-    // Sadece metne odaklanmak yerine, içinde "bulunmamaktadır" geçen her şeyi kabul et
     cy.contains(/ürün bulunmamaktadır|sepetiniz boş/i, { timeout: 10000 }).should('be.visible');
 }
 }
-
 export default new CartPage();
